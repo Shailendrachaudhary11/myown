416e72961cec6f74151e12ad629cf5681ba8b6f8
Shailendrachaudhary11 <shailendrachaurdhary0008@gmail.com>
2025-01-18 11:11:20 +0530

    This sub file

--- sub.java
+++ sub.java
@@ -1,5 +1,8 @@
 class sub{
     public static void main(String[] args) {
-        System.out.println("This is sub branch file");
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+        System.out.println(" "+(a-b));
     }
 }
